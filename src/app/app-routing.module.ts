091e97dbd5a3b1421596678bb5000a61ddaf0203
George Zygmund <zygmundgw@cardiff.ac.uk>
2022-07-24 20:12:37 +0100

    Navbar links working and cursor now a pointer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {CheckoutComponent} from "./component/checkout/checkout.component";
 import {LoginComponent} from "./component/login/login.component";
 import {AdminProductsComponent} from "./component/admin/admin-products/admin-products.component";
 import {AdminOrdersComponent} from "./component/admin/admin-orders/admin-orders.component";
+import {MyOrdersComponent} from "./component/my-orders/my-orders.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'check-out', component: CheckoutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin/products', component: AdminProductsComponent },
-  { path: 'admin/orders', component: AdminOrdersComponent }
+  { path: 'admin/orders', component: AdminOrdersComponent },
+  { path: 'my-orders', component: MyOrdersComponent }
 ]
 
 @NgModule({
